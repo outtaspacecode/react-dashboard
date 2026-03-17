582e787225cedc8302bb23dfc207670bbc7485a1
outtaspacecode <cole.beebe@protonmail.com>
2026-03-16 21:28:10 -0400

    Fixed global header icons; adjusted light/dark theming

--- src/components/headers/GlobalHeader.jsx
+++ src/components/headers/GlobalHeader.jsx
@@ -1,6 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import '../../styles/headers/GlobalHeader.css';
-import navIcons from '../../assets/nav.sprite.svg';
+import { GoHome } from 'react-icons/go';
+import { TbLayoutGrid } from 'react-icons/tb';
+import { CiCalendar } from 'react-icons/ci';
+import { FiLogOut } from 'react-icons/fi';
+import { IoSettingsOutline } from 'react-icons/io5';
 
 function GlobalHeader() {
   return (
@@ -8,26 +12,21 @@ function GlobalHeader() {
       <nav>
         <div className="top-links">
           <NavLink to="/" className={({ isActive }) => isActive ? 'active-link': ''}>
-            <svg width="30" height="30" role="presentation">
-              <use href={`${navIcons}#home`} />
-            </svg>
+            <GoHome size={30} strokeWidth={0.3} />
           </NavLink>
           <NavLink to="/todo" className={({ isActive }) => isActive ? 'active-link': ''}>
-            <svg width="30" height="30" role="presentation">
-              <use href={`${navIcons}#grid`} />
-            </svg>
+            <TbLayoutGrid size={30} strokeWidth={1.8} />
           </NavLink>
           <NavLink to="/todo">
-            <svg width="30" height="30" role="presentation">
-              <use href={`${navIcons}#calendar`} />
-            </svg>
+            <CiCalendar size={30} strokeWidth={0.75} />
           </NavLink>
         </div>
         <div className="bottom-links">
           <NavLink to="/">
-            <svg width="30" height="30" role="presentation">
-              <use href={`${navIcons}#settings`} />
-            </svg>
+            <IoSettingsOutline size={30} />
+          </NavLink>
+          <NavLink to="/">
+            <FiLogOut size={25} strokeWidth={2} />
           </NavLink>
         </div>
       </nav>
